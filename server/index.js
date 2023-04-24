4d2882ecd9166d163600eae9240cc2feb089e7c4
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-04-24 17:39:45 +0530

    Clutster : Cluster is added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,40 @@
+const cluster = require("cluster");
 const express = require("express");
+const bodyParser = require("body-parser");
 
-const app = express();
+const { connection } = require("./config/db");
 
-app.get("/", (req, res) => {
-  res.send("Working");
-});
+const numCPU = require("os").cpus().length;
 
-app.listen(8080, () => {
-  console.log("database connected");
-});
+if (cluster.isMaster) {
+  console.log(`Master process is running with pid ${process.pid}`);
+
+  // Fork workers
+  for (let i = 0; i < numCPU; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.send("Working");
+  });
+
+  app.listen(8080, async () => {
+    try {
+      await connection;
+      console.log("Database connection established");
+      console.log(`Worker process ${process.pid} is running`);
+    } catch (error) {
+      console.error(`Error connecting to database: ${error}`);
+    }
+  });
+}
