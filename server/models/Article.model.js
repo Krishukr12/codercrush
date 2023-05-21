316cef8435185845aaf67cc8e9cd844dfd681e5d
Krishan Kumar Safi <99599853+Krishukr12@users.noreply.github.com>
2023-05-21 11:19:57 +0530

    Adding timestamps to article model

--- server/models/Article.model.js
+++ server/models/Article.model.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const articleSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  image: { type: String, required: true },
-  body: { type: String, required: true },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "category",
-    required: true,
+const articleSchema = mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    image: { type: String, required: true },
+    body: { type: String, required: true },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "category",
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const ArticleModel = mongoose.model("article", articleSchema);
 module.exports = {
